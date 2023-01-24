3252c274ef52ff27aeb1f1264521ed61937b0093
PRYAMOI <usarov212@gmail.com>
2023-01-24 20:15:43 +0500

    Работа со связанными таблицами. Аутентификация пользователей

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 date_default_timezone_set('Asia/Yekaterinburg');
 require "dbconnect.php";
-require "event.php";
 require "auth.php";
 require "menu.php";
 if (isset($_SESSION['login'])){
     require "event.php";
 }
 else{
-    echo 'Войдите в сиситему для просмотра и создания событий';
+    echo 'Войдите в систему для просмотра и создания событий';
 }
